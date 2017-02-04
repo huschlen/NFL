5cabcf821dae88eb18ca5f48a42d4241746eaecc
huschlen <huschlen@comcast.net>
2017-02-04 07:45:36 -0600

    filter to fix index

---
+++ src/main/webapp/resources/js/filters/indexFilter.js
@@ -0,0 +1,12 @@
+angular
+	.module('nfl')
+	.filter('index', function () {
+		return function (array, index) {
+			if (!index)
+			index = 'index';
+			for (var i = 0; i < array.length; ++i) {
+				array[i][index] = i;
+			}
+			return array;
+	};
+});
