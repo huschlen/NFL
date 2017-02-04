edba2e0f5cd0d8d2a078ce56598831b744508f7c
huschlen <huschlen@comcast.net>
2017-02-04 07:46:04 -0600

    corrected the parameter

--- src/main/webapp/resources/js/filters/round1Filter.js
+++ src/main/webapp/resources/js/filters/round1Filter.js
@@ -1,13 +1,15 @@
 angular
 	.module('nfl')
 	.filter('round1Filter', function() {
-		return function(teams) {
+		return function(teams, team) {
 			var filtered = [];
-			angular.forEach(teams, function(myTeam) {
-				if(myTeam.go == 1 && myTeam.roundPlayed == 0) {		
-					filtered.push(myTeam);
+			angular.forEach(teams, function(team) {
+				console.log("team.roundPlayed: "+team.roundPlayed);
+				if(team.roundPlayed == 0) {		
+					filtered.push(team);
 				}
 			});
+			console.log(filtered);
 			return filtered;
 		}
 	});
