e0077840793a03ea8008b9ca18eec12af472308a
huschlen <huschlen@comcast.net>
2017-02-07 19:43:12 -0600

    Removed logging.

--- src/main/webapp/resources/js/filters/conferenceChampionshipFilter.js
+++ src/main/webapp/resources/js/filters/conferenceChampionshipFilter.js
@@ -4,12 +4,10 @@ angular
 		return function(teams, team) {
 			var filtered = [];
 			angular.forEach(teams, function(team) {
-				console.log(team.name);
 				if((team.go == 1 && team.roundPlayed == 1) || (team.roundPlayed == 2)) {
 					filtered.push(team);
 				}
 			});
-			console.log(filtered);
 			return filtered;
 		}
 	});
